025b4801897cb495c48fb5629abbdf8f0081b997
Jonathan <60202293+AkkozaDevelops@users.noreply.github.com>
2020-11-10 14:30:50 -0500

    Update richpresence.py

--- richpresence.py
+++ richpresence.py
@@ -2,29 +2,25 @@ from pypresence import Presence
 import psutil
 import time
 
-client_id = "775637920333037579"  # Put your Client ID in here
+client_id = "CLIENT ID HERE"
 RPC = Presence(client_id)
 
 RPC.connect()
 
 while True:
     cpu_per = round(psutil.cpu_percent(),1)
-    print(psutil.cpu_freq())
-    cpu_freq = round(psutil.cpu_freq().current)/1000
+    cpu_freq = psutil.cpu_freq().current/1000
     mem = psutil.virtual_memory()
     mem_per = round(psutil.virtual_memory().percent,1)
 
-    print(
-        RPC.update(
-            details="RAM: "+str(mem_per)+"%", 
-            state="CPU: "+str(cpu_per)+"% @ " + str(cpu_freq) + "GHz",
-            large_image="emcjforu4aamc8k",
-            large_text="I simp for Gamer Gura.",
-            small_image="shaark",
-            small_text="Gura is hydrodynamic!",
-            party_id="Fake Party ID!",
-            join="Fake Join ID!",
-            #start=start
-        )
+    RPC.update(
+        details="RAM: "+str(mem_per)+"%", 
+        state="CPU: "+str(cpu_per)+"% @ " + str(cpu_freq) + "GHz",
+        large_image="CHANGE ME TO BIG IMAGE KEY",
+        large_text="CHANGE ME TO WHAT YOU WANT HOVER TEXT TO BE",
+        small_image="CHANGE ME TO SMALL IMAGE KEY",
+        small_text="CHANGE ME TO WHAT YOU WANT HOVER TEXT TO BE",
+        party_id="Fake Party ID!",
+        join="Fake Join ID!"
     )
     time.sleep(15)
