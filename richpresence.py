65e7f312c4da12c0288b6e50d7ee32613e786a5a
Jonathan <60202293+AkkozaDevelops@users.noreply.github.com>
2021-03-01 11:10:34 -0500

    fixed None + None
    fixed my discord

--- richpresence.py
+++ richpresence.py
@@ -1,5 +1,5 @@
 
-print("Created by AkkozaDevelops\n\nGitHub Repo: https://github.com/AkkozaDevelops/PCStats-Rich-Presence\n\nIssues? you can either\nAdd me on Discord [Akkoza#8767]\nOR\nCreate a issue request on the Github Repo")
+print("Created by AkkozaDevelops\n\nGitHub Repo: https://github.com/AkkozaDevelops/PCStats-Rich-Presence\n\nIssues? you can either\nAdd me on Discord [Akkoza#3400]\nOR\nCreate a issue request on the Github Repo")
 # You don't have to keep this, but I would rather you do <3
 
 import time
