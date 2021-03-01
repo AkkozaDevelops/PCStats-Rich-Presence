4c40c11ed7428edc03b8337a9d44bd80e26e5bda
Jonathan <60202293+AkkozaDevelops@users.noreply.github.com>
2021-03-01 11:19:42 -0500

    test
    testing a new thing

--- richpresence.py
+++ richpresence.py
@@ -130,8 +130,7 @@ if not (config == None):
         if topText == "":
             topText = None
 
-        try:
-            RPC.update(
+        table = {
                     details=topText,
                     state=bottomText,
                     large_image=config["images"]["large_image"],
@@ -139,9 +138,14 @@ if not (config == None):
                     small_image=config["images"]["small_image"],
                     small_text=config["hover_text"]["small_image_text"],
                     party_id=gameId,
-                    join=gameId or "" + gameId or "",
                     start=start_time
-            )
+        }
+        
+        if gameId != None:
+            table.join = gameId + gameId
+            
+        try:
+            RPC.update(table)
 
             time.sleep(15)
         except Exception as e:
